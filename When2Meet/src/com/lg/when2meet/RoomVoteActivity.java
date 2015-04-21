da7820af9a33070964ca4f942c72c51dbb6a5d59
project <project@LGD-A16>
2015-04-21 16:03:39 +0900

    roomvote

---
+++ When2Meet/src/com/lg/when2meet/RoomVoteActivity.java
@@ -0,0 +1,15 @@
+package com.lg.when2meet;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class RoomVoteActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_room_vote);
+	}
+}
