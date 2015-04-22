6391bc503142f46761b402851b36a4602c202f4c
uniony <yoonhj9153@gmail.com>
2015-04-22 15:11:38 +0900

    자동 로그인 기능 추가

--- When2Meet/src/com/lg/when2meet/LoginActivity.java
+++ When2Meet/src/com/lg/when2meet/LoginActivity.java
@@ -19,6 +19,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -28,7 +29,18 @@ public class LoginActivity extends Activity {
 	ImageView join;
 	EditText login_id;
 	EditText login_pwd;
-	
+	CheckBox auto_login;
+
+	String loginId;
+	String loginPwd;
+
+	SharedPreferences setting;
+	SharedPreferences.Editor editor;
+	String result;
+	Boolean isAuto;
+
+	public static final String LOGIN_PREFRENCE = "login_pref";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -38,87 +50,136 @@ public class LoginActivity extends Activity {
 		join = (ImageView) findViewById(R.id.join);
 		login_id = (EditText) findViewById(R.id.login_id);
 		login_pwd = (EditText) findViewById(R.id.login_pwd);
-		
-		final Handler handler = new Handler(){
+		auto_login = (CheckBox) findViewById(R.id.auto_login);
+
+		final Handler handler = new Handler() {
 			public void handleMessage(android.os.Message msg) {
-				Toast.makeText(LoginActivity.this, "아이디나 비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show();
+				Toast.makeText(LoginActivity.this, "아이디나 비밀번호가 일치하지 않습니다",
+						Toast.LENGTH_SHORT).show();
 			};
 		};
-		
-		login.setOnClickListener(new View.OnClickListener() {
 
-			@Override
-			public void onClick(View v) {				
-				String loginId = login_id.getText().toString();
-				String loginPwd = login_pwd.getText().toString();
-				
-				if(loginId.equals("") || loginId == null){
-					Toast.makeText(LoginActivity.this, "아이디를 입력하세요", Toast.LENGTH_SHORT).show();
-				}else if(loginPwd.equals("") || loginPwd == null){
-					Toast.makeText(LoginActivity.this, "비밀번호를 입력하세요", Toast.LENGTH_SHORT).show();
-				}
-				
-				new Thread(){
-					@Override
-					public void run() {
-						String loginId = login_id.getText().toString();
-						String loginPwd = login_pwd.getText().toString();
-						
-						String result = SendByHttpLogin(loginId, loginPwd);
-						
-						SharedPreferences setting = getSharedPreferences("login", 0);
-						SharedPreferences.Editor editor = setting.edit();
-						
-						try {
-							JSONObject json = new JSONObject(result);
-							String isSuccess = json.getString("isSuccess");
-							
-							if(isSuccess.equals("true")){
-								editor.putString("phoneNo", loginId);
-								editor.putString("pwd", loginPwd);
-								editor.putString("id", json.getString("id"));
-								editor.commit();
-								Intent intent = new Intent(getApplicationContext(),
-										ListActivity.class);
-								intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-								intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-								startActivity(intent);
-							}else{
-								handler.sendMessage(Message.obtain());
+
+		final Handler connectErr = new Handler() { public void
+			handleMessage(android.os.Message msg) {
+			Toast.makeText(LoginActivity.this, "접속 오류 입니다. 잠시 후 시도해주세요.",
+					Toast.LENGTH_SHORT).show(); }; };
+
+
+					setting = getSharedPreferences("LOGIN_PREFRENCE", 0);
+					isAuto = setting.getBoolean("isAuto", false);
+					Log.d("check", "bool "+isAuto);
+
+					if (isAuto){
+						new Thread() {
+							@Override
+							public void run() {
+
+								loginId = setting.getString("phoneNo", "");
+								loginPwd = setting.getString("pwd", "");
+								result = SendByHttpLogin(loginId, loginPwd);
+
+								try {
+									JSONObject json = new JSONObject(result);
+									String isSuccess = json.getString("isSuccess");
+
+									if (isSuccess.equals("true")) {
+										Intent intent = new Intent(
+												getApplicationContext(),
+												ListActivity.class);
+										intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+										intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+										startActivity(intent);
+									} else {
+										connectErr.sendMessage(Message.obtain());
+									}
+
+								} catch (JSONException e) {
+									e.printStackTrace();
+								}
+							};
+						}.start();
+					}
+
+					login.setOnClickListener(new View.OnClickListener() {
+
+						@Override
+						public void onClick(View v) {
+							loginId = login_id.getText().toString();
+							loginPwd = login_pwd.getText().toString();
+
+							if (loginId.equals("") || loginId == null) {
+								Toast.makeText(LoginActivity.this, "아이디를 입력하세요",
+										Toast.LENGTH_SHORT).show();
+							} else if (loginPwd.equals("") || loginPwd == null) {
+								Toast.makeText(LoginActivity.this, "비밀번호를 입력하세요",
+										Toast.LENGTH_SHORT).show();
+							} else {
+
+								new Thread() {
+									@Override
+									public void run() {
+
+										result = SendByHttpLogin(loginId, loginPwd);
+										editor = setting.edit();
+
+										try {
+											JSONObject json = new JSONObject(result);
+											String isSuccess = json.getString("isSuccess");
+
+											if (isSuccess.equals("true")) {
+												if(auto_login.isChecked()) {
+													editor.putString("phoneNo", loginId);
+													editor.putString("pwd", loginPwd);
+													editor.putBoolean("isAuto", true);
+													editor.commit();	
+													Log.d("check", "after commit: "+ setting.getBoolean("isAuto", false));
+												} else {
+													editor.clear();
+													editor.commit();
+												}
+												
+												Intent intent = new Intent(
+														getApplicationContext(),
+														ListActivity.class);
+												intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+												intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+												startActivity(intent);
+											} else {
+												handler.sendMessage(Message.obtain());
+											}
+
+										} catch (JSONException e) {
+											e.printStackTrace();
+										}
+									};
+								}.start();
 							}
-						
-						} catch (JSONException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
 						}
-						//String[][] parsedData = jsonParserList(result);
-					};
-				}.start();
-				
-			}
-		});
+					});
 
-		join.setOnClickListener(new View.OnClickListener() {
+					join.setOnClickListener(new View.OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(getApplicationContext(),
-						JoinActivity.class);
-				startActivity(intent);
-			}
-		});
+						@Override
+						public void onClick(View v) {
+							Intent intent = new Intent(getApplicationContext(),
+									JoinActivity.class);
+							startActivity(intent);
+						}
+					});
 	}
-	
+
 	/**
 	 * 서버에 데이터를 보내는 메소드
 	 */
 	private String SendByHttpLogin(String phoneNo, String pwd) {
 		String URL = "http://192.168.0.130:8080/checkJoin";
-		
+
 		DefaultHttpClient client = new DefaultHttpClient();
 		try {
 			/* 체크할 id와 pwd값 서버로 전송 */
-			HttpPost post = new HttpPost(URL+"?phoneNo="+phoneNo+"&pwd="+pwd);
+			HttpPost post = new HttpPost(URL + "?phoneNo=" + phoneNo + "&pwd="
+					+ pwd);
 
 			/* 지연시간 최대 5초 */
 			HttpParams params = client.getParams();
@@ -137,13 +198,13 @@ public class LoginActivity extends Activity {
 			while ((line = bufreader.readLine()) != null) {
 				result += line;
 			}
-			
+
 			return result;
 		} catch (Exception e) {
 			e.printStackTrace();
-			client.getConnectionManager().shutdown();	// 연결 지연 종료
-			return ""; 
+			client.getConnectionManager().shutdown(); // 연결 지연 종료
+			return "";
 		}
-		
+
 	}
 }
